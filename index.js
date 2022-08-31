cbd286d7765962c57c2c4c9cd903f0954efbcb1c
Yurij Mikhalevich <yurij@grid.ai>
2022-08-31 21:13:36 +0300

    `Intl.DisplayNames` on `iOS 14`

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-// Make a network request using fetch
-// This is not an error because we added a fetch polyfill
-// in our eslint settings
-fetch('google.com');
-
-// Instantiate PaymentRequest
-const req = new PaymentRequest();
-
-// Instantiate IntersectionObserver
-const observer = new IntersectionObserver(() => {});
-
-const foo = Object.values({});
+const displayNames = new Intl.DisplayNames(["en"], { type: "region" });
